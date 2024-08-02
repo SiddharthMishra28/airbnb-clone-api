ed1a7ea11bb91f1c5049e85c3095c2273c8b850c
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-08-02 23:59:44 +0530

    corrected reiew route import

--- src/routes/ReviewRoutes.js
+++ src/routes/ReviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const reviewController = require('../controllers/reviewController');
+const reviewController = require('../controllers/ReviewController');
 const authenticateToken = require('../middleware/AuthMiddleware');
 const {
     validateReviewCreation,
